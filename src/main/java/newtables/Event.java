ef96d053234188d29eb2e3924c008aa92bf374cf
saara-h <143730510+saara-h@users.noreply.github.com>
2023-12-21 21:04:01 +0200

    yhteyksiä eventtiin

--- src/main/java/newtables/Event.java
+++ src/main/java/newtables/Event.java
@@ -3,18 +3,17 @@ package newtables;
 import java.sql.Date;
 import java.sql.Time;
 
-import javax.persistence.Entity;
-
-import org.springframework.data.jpa.domain.AbstractPersistable;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Event extends AbstractPersistable<Long> {
 
     private String eventTitle;
@@ -22,5 +21,9 @@ public class Event extends AbstractPersistable<Long> {
     private Date eventDate;
     private Time eventTime;
     private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
     
 }
